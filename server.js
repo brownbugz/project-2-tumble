00e10be9576d51c1a7eef966cf428d1d64286c99
sonniakemmer <brownbugz@gmail.com>
2019-08-23 15:05:39 -0700

    update server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
 
 require('dotenv').config();
 
@@ -22,7 +23,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  secret: 'toDateOrNotToDate',
+  resave: false,
+  saveUninitialized: true
+}))
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', indexRouter);
 app.use('/tumblers', tumblersRouter);
