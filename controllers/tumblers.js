5e782452a3d9a3e5bf3fd2059ebea06e17caa5b9
sonniakemmer <brownbugz@gmail.com>
2019-08-26 10:47:51 -0700

    cleanup and update

--- controllers/tumblers.js
+++
@@ -1,9 +0,0 @@
-// const Tumbler = require('../models/tumbler');
-
-// module.exports = {
-//     index,
-// }
-
-// function index(req, res, next) {
-//     res.render('/index');
-// }
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var indexController = require('../controllers/index');
 
 /* GET home page while logged out - this is the start page */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Tumble' });
+router.get('/', indexController.index);
+
+router.get('/auth/google', passport.authenticate(
+  'google',
+  { scope:['profile', 'email'] }
+));
+
+router.get('/oauth2callback', passport.authenticate(
+  'google',
+  {
+    successRedirect: '/users-homepage',
+    failureRedirect: '/'
+  }
+));
+
+router.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
 });
 
 module.exports = router;
